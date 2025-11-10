cdc2283023ffcd57072611bcc327faef63bffe79
Forgács Alex <forgacs.alex@diak.szbi-pg.hu>
2025-11-10 11:06:27 +0100

    add.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function switchTheme() {
-  // TODO: implement toggling the light/dark theme
+  document.body.classList.add('bg-dark')
+
+  const navbar = document.getElementById('navbar')
+  const cards = document.querySelectorAll('.card')
+
+  navbar.classList.remove('navbar-light', 'bg-light')
+  navbar.classList.add('navbar-dark', 'bg-dark')
+
+  cards.forEach(card => {
+    card.classList.add("text-white", "bg-dark", "border-light")
+  })
 }
